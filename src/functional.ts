0cde7c27e3be85132aa0d684829321d33173b69c
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-25 11:21:27 -0300

    fix(functional): Export not from functional module

--- src/functional.ts
+++ src/functional.ts
@@ -1,3 +1,4 @@
 export * as operators from "./functional/operators.js";
 export { compose } from "./functional/compose.js";
 export { identity } from "./functional/identity.js";
+export { not } from "./functional/not.js";
