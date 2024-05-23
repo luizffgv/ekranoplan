84862b79586b9866fddad0bab7ff7f8e8077d8e4
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-05-23 19:34:36 -0300

    docs: Change docs theme color

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,6 +1,7 @@
-/** @type {Partial<import('typedoc').TypeDocOptions>} */
+/** @type {Partial<import('typedoc').TypeDocOptions & {themeColor: string}>} */
 export default {
   entryPoints: ["src/index.ts"],
   tsconfig: "tsconfig.json",
   plugin: ["typedoc-material-theme"],
+  themeColor: "#e6e7e9",
 };
