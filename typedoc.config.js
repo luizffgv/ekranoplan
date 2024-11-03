bdb0ea480300c4495bd696d92660da97fb4976d4
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-11-03 15:40:42 -0300

    fix: change typedoc cfg to json

--- typedoc.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {Partial<import('typedoc').TypeDocOptions & {themeColor: string}>} */
-export default {
-  entryPoints: ["src/"],
-  entryPointStrategy: "expand",
-  exclude: ["**/*.test.ts"],
-  tsconfig: "tsconfig.json",
-  plugin: ["typedoc-material-theme"],
-  themeColor: "#e6e7e9",
-};
